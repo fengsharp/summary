a159e1b7f41d9f5689fb297d829dc46b6bce7789
feng <canfeng12123@126.com>
2021-08-23 16:04:40 +0800

    for upper

--- 02c/code03/practice01/main.c
+++ 02c/code03/practice01/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "dlist.h"
 
@@ -24,18 +26,16 @@ void dlist_int_sum(void *ctx, void *data)
 
 void dlist_int_max(void *ctx, void *data)
 {
-    int *pMax = (int*)(ctx);
-    int *pNum = (int*)(data);
+    int *pMax = (int *)(ctx);
+    int *pNum = (int *)(data);
     if (*pMax < *pNum)
     {
         *pMax = *pNum;
     }
 }
 
-int main()
+void dlist_int_test()
 {
-    puts("--- start ---");
-
     // 10 num
     DList *dlist = dlist_create();
     if (dlist == NULL)
@@ -63,11 +63,60 @@ int main()
     int max = 0;
     dlist_foreach(dlist, dlist_int_max, &max);
     printf("max: %d\n", max);
-    
+
     // release
     dlist_foreach(dlist, dlist_int_destroy, NULL);
     dlist_destroy(dlist);
+}
 
+void dlist_str_print(void *ctx, void *data)
+{
+    char *str = (char *)(data);
+    printf("%s ", str);
+}
+
+void dlist_str_destry(void *ctx, void *data)
+{
+    free(data);
+}
+
+void dlist_str_toupper(void *ctx, void *data)
+{
+    char *str = (char *)(data);
+    char *pCursor = str;
+    while (*pCursor != '\0')
+    {
+        if (islower(*pCursor))
+        {
+            *pCursor = toupper(*pCursor);
+        }
+        ++pCursor;
+    }
+}
+
+void dlist_str_test()
+{
+    DList *dlist = dlist_create();
+
+    dlist_push_back(dlist, strdup("hello"));
+    dlist_push_back(dlist, strdup("world"));
+
+    dlist_foreach(dlist, dlist_str_print, NULL);
+    puts("");
+
+    dlist_foreach(dlist, dlist_str_toupper, NULL);
+    dlist_foreach(dlist, dlist_str_print, NULL);
+    puts("");
+
+    // release
+    dlist_foreach(dlist, dlist_str_destry, NULL);
+    dlist_destroy(dlist);
+}
+
+int main()
+{
+    puts("--- start ---");
+    dlist_str_test();
     puts("--- end ---");
 
     return 0;
