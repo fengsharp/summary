5a25881b5aef4ffbc34fe19f9337a68054e315eb
feng <canfeng12123@126.com>
2021-08-30 16:36:26 +0800

    update

---
+++ 02c/code03/practice07/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+    <?xml version="1.0" encoding="utf-8"?>
+    <!--comment-->
+    <br />
+    <p>ppp</p>
+    <br />
+    <programmer name="lixianjing" blog="http://www.limodev.cn/blog">text</programmer>
+*/
+
+int main()
+{
+
+    return 0;
+}
---
+++ 02c/code03/practice07/xml_builder_dump.h
@@ -0,0 +1,8 @@
+#ifndef XML_BUILDER_DUMP_H
+#define XML_BUILDER_DUMP_H
+
+#include "xml_builder.h"
+
+XmlBuilder* xml_builder_dump_create(FILE* fp);
+
+#endif
